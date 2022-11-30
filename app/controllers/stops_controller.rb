9e76b24afde51a3d1f62e0921c4bd47257be2a99
mosesnjoroge <mosesbnjoroge@outlook.com>
2022-11-30 11:49:52 -0500

    further work on the stops controller

--- app/controllers/stops_controller.rb
+++ app/controllers/stops_controller.rb
@@ -1,28 +1,46 @@
 class StopsController < ApplicationController
   before_action :set_sidequest
   before_action :set_trip
+  before_action :set_stop
 
-  def new
-    @stop = Stop.new
-  end
-
-  def create
-
+  def index
+    @stops = Stop.all
   end
 
   def show
+    @stop = Stop.find(param[:id])
+  end
+
+  def new
+    @stop = Stop.new
+    @category = Trip.new
+    @location = Location.new
+  end
+
+  def create
+    @stop = current_user.sidequests
+    #if @stop.save
 
   end
+
 
   def edit
 
   end
 
   def destroy
-
+    @stop.destroy
   end
 
   private
+
+  def stop_params
+    params.require(:stop).permit(:trip)
+  end
+
+  def set_stop
+    @stop = Stop.find_by(trip_id: params[:id])
+  end
 
   def set_sidequest
     @sidequest = SideQuest.find(params[:id])
