9093605031a5b57979fc03ef56d8f0e167d220f5
mosesnjoroge <mosesbnjoroge@outlook.com>
2022-11-30 11:36:42 -0500

    generated template for controlller methods

--- app/controllers/stops_controller.rb
+++ app/controllers/stops_controller.rb
@@ -1,2 +1,35 @@
 class StopsController < ApplicationController
+  before_action :set_sidequest
+  before_action :set_trip
+
+  def new
+    @stop = Stop.new
+  end
+
+  def create
+
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def set_sidequest
+    @sidequest = SideQuest.find(params[:id])
+  end
+
+  def set_trip
+    @trip = Trip.find(params[:id])
+  end
+
 end
